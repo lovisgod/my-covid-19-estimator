0c96fdb86f1b5532794e5630d0aa0a41f1bcae0e
Lovisgod <olifedayo94@gmail.com>
2020-04-07 21:40:37 +0100

    BG/failing test

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,7 @@
 const covid19ImpactEstimator = (data) => {
   console.log(data);
   const {
+    periodType,
     reportedCases,
     timeToElapse
   } = data;
@@ -12,7 +13,21 @@ const covid19ImpactEstimator = (data) => {
   // calculate the number of currently infected for both impact and severe impact
   const currentlyInfected = (factor) => reportedCases * factor;
   const infectionsByRequestedTime = (time) => {
-    const multiplier = 2 ** (time / 3);
+    let t = time;
+    switch (periodType) {
+      case 'days':
+        t *= 1;
+        break;
+      case 'weeks':
+        t *= 7;
+        break;
+      case 'months':
+        t *= 31;
+        break;
+      default:
+        break;
+    }
+    const multiplier = 2 ** (t / 3);
     return multiplier;
   };
   result.impact.currentlyInfected = currentlyInfected(10);
