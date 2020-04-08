96832fc14ce0653aba1c55ea2b97108eece334d1
Lovisgod <olifedayo94@gmail.com>
2020-04-08 10:56:32 +0100

    FT/challenge three

--- src/estimator.js
+++ src/estimator.js
@@ -3,40 +3,42 @@ const covid19ImpactEstimator = (data) => {
     reportedCases,
     timeToElapse,
     periodType,
-    totalHospitalBeds
+    totalHospitalBeds,
+    region
   } = data;
   const result = {
     data,
     impact: {},
     severeImpact: {}
   };
+
+  let t = timeToElapse;
+  switch (periodType) {
+    case 'days':
+      t *= 1;
+      break;
+    case 'weeks':
+      t *= 7;
+      break;
+    case 'months':
+      t *= 31;
+      break;
+    default:
+      break;
+  }
   // calculate the number of currently infected for both impact and severe impact
   const currentlyInfected = (factor) => reportedCases * factor;
   const infectionsByRequestedTime = (time) => {
-    let t = time;
-    switch (periodType) {
-      case 'days':
-        t *= 1;
-        break;
-      case 'weeks':
-        t *= 7;
-        break;
-      case 'months':
-        t *= 31;
-        break;
-      default:
-        break;
-    }
-    const multiplier = 2 ** Math.floor(t / 3);
+    const multiplier = 2 ** Math.floor(time / 3);
     return multiplier;
   };
   result.impact.currentlyInfected = currentlyInfected(10);
   result.severeImpact.currentlyInfected = currentlyInfected(50);
   result.impact.infectionsByRequestedTime = (
-    result.impact.currentlyInfected * infectionsByRequestedTime(timeToElapse)
+    result.impact.currentlyInfected * infectionsByRequestedTime(t)
   );
   result.severeImpact.infectionsByRequestedTime = (
-    result.severeImpact.currentlyInfected * infectionsByRequestedTime(timeToElapse)
+    result.severeImpact.currentlyInfected * infectionsByRequestedTime(t)
   );
   result.impact.severeCasesByRequestedTime = result.impact.infectionsByRequestedTime * 0.15;
   result.severeImpact.severeCasesByRequestedTime = (
@@ -48,15 +50,43 @@ const covid19ImpactEstimator = (data) => {
   result.severeImpact.hospitalBedsByRequestedTime = (
     (0.35 * totalHospitalBeds) - result.severeImpact.severeCasesByRequestedTime
   );
+  result.impact.casesForICUByRequestedTime = (
+    result.impact.infectionsByRequestedTime * 0.05
+  );
+  result.severeImpact.casesForICUByRequestedTime = (
+    result.severeImpact.infectionsByRequestedTime * 0.05
+  );
+  result.impact.casesForVentilatorsByRequestedTime = (
+    result.impact.infectionsByRequestedTime * 0.02
+  );
+  result.severeImpact.casesForVentilatorsByRequestedTime = (
+    result.severeImpact.infectionsByRequestedTime * 0.02
+  );
+  result.impact.dollarsInFlight = (
+    (
+      result.impact.infectionsByRequestedTime * region.avgDailyIncomePopulation)
+      * region.avgDailyIncomeInUSD * t
+  );
+  result.severeImpact.dollarsInFlight = (
+    (
+      result.severeImpact.infectionsByRequestedTime * region.avgDailyIncomePopulation)
+      * region.avgDailyIncomeInUSD * t
+  );
   console.log(result);
   return result;
 };
 
 // covid19ImpactEstimator({
-//   reportedCases: 4109,
-//   population: 41503276,
-//   totalHospitalBeds: 434486,
-//   timeToElapse: 64,
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.72
+//   },
+//   reportedCases: 3942,
+//   population: 149744335,
+//   totalHospitalBeds: 358078,
+//   timeToElapse: 32,
 //   periodType: 'days'
 // });
 
