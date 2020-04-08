79e987fbc366029bf98a17c350074db2fd5db23e
Lovisgod <olifedayo94@gmail.com>
2020-04-08 23:11:08 +0100

    FT/challenge 3

--- src/router.js
+++ src/router.js
@@ -6,4 +6,7 @@ router.post('/on-covid-19/:type', handleRequest);
 router.post('/on-covid-19', handleRequest);
 router.get('/on-covid-19/logs', getLogs);
 
+router.get('/', (req, res) => {
+  res.status(200).send('Welcome home');
+});
 module.exports = router;
